11bd9203fea041091d454a5ffb6804546cac165f
neDmitry-sleep <verygadverygad@gmail.com>
2026-04-09 11:28:38 +0700

    Add taskId/schedule route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CalendarView from './pages/CalendarView';
 import TaskList from './pages/TaskList';
 import Statistics from './pages/Statistics';
 import NewTask from './pages/NewTask';
+import TaskSchedule from './pages/TaskSchedule';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -27,6 +28,7 @@ function App() {
             <Route path="/calendar" element={<CalendarView />} />
             <Route path="/tasks" element={<TaskList />} />
             <Route path="/tasks/new" element={<NewTask />} />
+            <Route path="/tasks/:taskId/schedule" element={<TaskSchedule />} />
             <Route path="/stats" element={<Statistics />} />
           </Routes>
 
